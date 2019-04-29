611642633948a5d71fb2c226c07ebe2cc3b07e84
Lena Hamilton <hamiltonlena21@gmail.com>
2019-04-29 13:53:20 -0600

    Updates

---
+++ precache-manifest.eeaf35702ef9ccebec3e510ad46124f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f3e1edbe9b2314b877646a762e1e5c",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "d4b9b7c3de77e5fbc424",
+    "url": "/Memory-Game/static/css/main.785e4026.chunk.css"
+  },
+  {
+    "revision": "0eb94bbe785648bf5c02",
+    "url": "/Memory-Game/static/js/2.3043ad1e.chunk.js"
+  },
+  {
+    "revision": "d4b9b7c3de77e5fbc424",
+    "url": "/Memory-Game/static/js/main.4b13c8e6.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  }
+]);
